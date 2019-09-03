81ac95ea68259a21aed7c517ad80d148fcc3f5b6
Bobby Khounphinith <khoupe07@gmail.com>
2019-09-02 21:29:52 -0500

    Created Update and Delete routes in tasks.router file.

--- server/routes/tasks.router.js
+++ server/routes/tasks.router.js
@@ -2,8 +2,9 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
+// Get everything from "todo" table from db
 router.get('/', (req, res) => {
-    let queryText = `SELECT * FROM "tasks"`;
+    let queryText = `SELECT * FROM "todo"`;
     pool.query(queryText)
         .then(result => {
             res.send(result.rows);
@@ -13,8 +14,9 @@ router.get('/', (req, res) => {
         });
 });
 
+// Post tasks in todo table
 router.post('/', (req, res) => {
-    let queryText = `INSERT INTO "tasks" ("tasks", "due_date")
+    let queryText = `INSERT INTO "todo" ("tasks", "due_date")
                     VALUES ($1, $2);`;
     pool.query(queryText, [req.body.tasks, req.body.due_date])
     .then(() => {
@@ -24,5 +26,33 @@ router.post('/', (req, res) => {
         res.sendStatus(500);
     });
 });
+
+// Update tasks
+router.put('/:id', (req, res) => {
+    let id = req.params.id;
+    console.log('Show id of tasks.', id);
+    let queryText = `UPDATE "todo" SET "tasks" = $1 WHERE "id" = $2;`;
+    pool.query(queryText, [req.body.tasks, id])
+    .then(result => {
+        res.sendStatus(200);
+    }).catch(error => {
+        console.log('Error in PUT route', error);
+        res.sendStatus(500);
+    });
+});
+
+// Delete tasks
+router.delete('/:id', (req, res) => {
+    let id = req.params.id;
+    console.log('Show id in delete', req.params.id);
+    let queryText = `DELETE FROM "todo" WHERE "tasks"."id" = $1;`;
+    pool.query(queryText, [id])
+    .then(result => {
+        res.sendStatus(200);
+    }).catch(error => {
+        console.log('Error in DELETE route', error);
+        res.sendStatus(500);
+    });
+})
 
 module.exports = router;
